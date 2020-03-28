3efc9feacace7c36648902223df0c22597a20779
Burhanuddin Lakdawala <blakdawa@uci.edu>
2020-03-27 20:33:57 -0700

    a temp marker and fixed location

--- src/app/screens/dashboard/dashboard.component.ts
+++ src/app/screens/dashboard/dashboard.component.ts
@@ -11,7 +11,8 @@ import { environment } from 'src/environments/environment';
 })
 export class DashboardComponent implements OnInit, AfterViewInit {
 
-  map: any;
+  map: google.maps.Map<HTMLElement>;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -20,11 +21,58 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     get('https://maps.googleapis.com/maps/api/js?key=' + environment.MAPS_API_KEY, () => {
       this.map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 33.6, lng: -117.8 },
-        zoom: 8
+        center: { lat: 34.052235, lng: -118.243683 },
+        zoom: 13
+      });
+
+      const wholesomeChoice = { lat: 33.664290, lng: -117.825285 };
+      const infoWindow: google.maps.InfoWindow = new google.maps.InfoWindow();
+
+      // Try HTML5 geolocation. Nah it only works on https
+      // if (navigator.geolocation) {
+      //   navigator.geolocation.getCurrentPosition((position) => {
+      //     const pos = {
+      //       lat: position.coords.latitude,
+      //       lng: position.coords.longitude
+      //     };
+
+      //     infoWindow.setPosition(pos);
+      //     infoWindow.setContent('Location found.');
+      //     infoWindow.open(this.map);
+      //     this.map.setCenter(pos);
+      //   }, () => {
+      //     this.handleLocationError(true, infoWindow, this.map.getCenter());
+      //   });
+      // } else {
+      //   // Browser doesn't support Geolocation
+      //   this.handleLocationError(false, infoWindow, this.map.getCenter());
+      // }
+      const currentPos = {
+        lat: 33.6704072,
+        lng: -117.8282598
+      };
+      const currentPosMarker = new google.maps.Marker({
+        position: currentPos,
+        map: this.map,
+        icon: 'http://www.robotwoods.com/dev/misc/bluecircle.png'
+      });
+      this.map.setCenter(currentPos);
+
+      // google.maps.SymbolPath.
+      const marker = new google.maps.Marker({
+        position: wholesomeChoice,
+        map: this.map
       });
     });
 
   }
 
+  handleLocationError(browserHasGeolocation: boolean, infoWindow: any, pos: any) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(this.map);
+  }
 }
+
