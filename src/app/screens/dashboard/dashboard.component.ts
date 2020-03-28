14fc8de11abbc9f4ee3a82cd0c4802b8ba2fa0a5
Burhanuddin Lakdawala <blakdawa@uci.edu>
2020-03-28 12:32:13 -0700

    sliders and colors incorporated

--- src/app/screens/dashboard/dashboard.component.ts
+++ src/app/screens/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/environments/environment';
 import tempJSON from 'src/assets/home_nearby.json';
 import { BackendService } from 'src/app/services/backend.service';
 import { Place } from 'src/app/models/place';
+import { MatSliderChange } from '@angular/material/slider';
 // declare var google: any;
 // Above not needed anymore as we edited tsconfig.app.json to include googlemaps types when compiling
 
@@ -17,9 +18,13 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   map: google.maps.Map<HTMLElement>;
   circles: google.maps.Circle[] = [];
   clickedLocationWindow: google.maps.InfoWindow = null;
+  clickedLatLng: google.maps.LatLng = null;
+  clickedCircle: google.maps.Circle = null;
 
   days: string[];
-  currentDay: string;
+  colors: string[];
+  // currentDate: Date;
+  currentDay: number;
   currentHour: number;
 
   busyMeter: string[];
@@ -27,11 +32,29 @@ export class DashboardComponent implements OnInit, AfterViewInit {
 
   constructor(private backend: BackendService) {
     this.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    this.colors = ['#FFFFFF', '#FFD700', '#FFA500', '#FF0000'];
     const currentDate = new Date();
-    this.currentDay = this.days[currentDate.getDay()];
+    this.currentDay = currentDate.getDay();
     this.currentHour = currentDate.getHours();
 
     this.busyMeter = ['Not crowded', 'A little crowded', 'Quite crowded', 'Very crowded'];
+  }
+
+  getDayFromIndex(index: number): string {
+    const days: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[index];
+  }
+
+  getHourFromIndex(index: number): string {
+    let h = index % 12;
+    if (h === 0) { h = 12; }
+    const partOfDay = Math.floor(index / 12) === 0 ? 'am' : 'pm';
+    return h + partOfDay;
+  }
+
+  changeInDateTime(e: MatSliderChange) {
+    console.log(e, this.currentDay, this.currentHour);
+    this.updateMapData(null);
   }
 
   ngOnInit(): void {
@@ -45,7 +68,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
       });
 
       // Try HTML5 geolocation. Nah it only works on https
-      const currentPos: google.maps.LatLng = new google.maps.LatLng(33.6704072, -117.8282598);
+      let currentPos: google.maps.LatLng = new google.maps.LatLng(33.6704072, -117.8282598);
       const currentPosMarker = new google.maps.Marker({
         position: currentPos,
         map: this.map,
@@ -53,7 +76,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
       });
       this.map.setCenter(currentPos);
 
-      this.backend.getNearbyPopularTimes(33.6471628, -117.8411294).subscribe((resp) => {
+      this.backend.getNearbyPopularTimes(currentPos.lat(), currentPos.lng()).subscribe((resp) => {
         console.log(resp);
         this.updateMapData(resp);
       });
@@ -62,7 +85,8 @@ export class DashboardComponent implements OnInit, AfterViewInit {
       this.map.addListener('idle', () => {
         if (!currentPos.equals(this.map.getCenter())) {
           // same code repeated as above
-          this.backend.getNearbyPopularTimes(33.6471628, -117.8411294).subscribe((resp) => {
+          currentPos = this.map.getCenter();
+          this.backend.getNearbyPopularTimes(currentPos.lat(), currentPos.lng()).subscribe((resp) => {
             console.log(resp);
             this.updateMapData(resp);
           });
@@ -76,22 +100,30 @@ export class DashboardComponent implements OnInit, AfterViewInit {
 
     for (const circle of this.circles) {
       google.maps.event.clearListeners(circle, 'click');
+      circle.setMap(null);
     }
     this.circles.length = 0;
-    if (this.clickedLocationWindow) { this.clickedLocationWindow.close(); }
 
-    this.places = Place.fromObjArr(resp);
+    if (resp != null) {
+      this.places = Place.fromObjArr(resp);
+    }
+
+    const currentDayText = this.days[this.currentDay];
     for (const place of this.places) {
 
-      const placeStatus = this.busyMeter[Math.floor(place.popularTimes.find(p => p.day === this.currentDay).times[this.currentHour] / 25)];
-      // console.log(place.popularTimes.find(p => p.day === this.currentDay).times[this.currentHour]);
+      let busynessIndex = Math.floor(place.popularTimes.find(p => p.day === currentDayText).times[this.currentHour] / 25);
+      // For the extreme cases, pop >= 100
+      if (busynessIndex > 3) { busynessIndex = 3; }
+      const placeStatus: string = this.busyMeter[busynessIndex];
+      const placeColor: string = this.colors[busynessIndex];
+      console.log(place.name, place.popularTimes.find(p => p.day === currentDayText).times[this.currentHour]);
 
 
       const circle = new google.maps.Circle({
-        strokeColor: '#FF0000',
+        strokeColor: placeColor,
         strokeOpacity: 0.8,
         strokeWeight: 2,
-        fillColor: '#FF0000',
+        fillColor: placeColor,
         fillOpacity: 0.35,
         map: this.map,
         center: place.coordinates,
@@ -101,14 +133,41 @@ export class DashboardComponent implements OnInit, AfterViewInit {
       google.maps.event.addListener(circle, 'click', (e) => {
         if (this.clickedLocationWindow) { this.clickedLocationWindow.close(); }
         this.clickedLocationWindow = new google.maps.InfoWindow();
-        this.clickedLocationWindow.setContent(`<b>${place.name}<\b><br>${placeStatus}`);
+        this.clickedLatLng = circle.getCenter();
+        this.clickedCircle = circle;
+        this.clickedLocationWindow.setContent(`<b>${place.name}</b><br>${placeStatus}`);
         this.clickedLocationWindow.setPosition(e.latLng);
         this.clickedLocationWindow.open(this.map);
       });
 
+      // google.maps.event.trigger();
+
       this.circles.push(circle);
     }
+
+    // const e = {
+    //   latLng: new google.maps.LatLng(33.6676238, -117.8292908)
+    // };
+    // google.maps.event.trigger(this.circles[0], 'click', e);
+
+
+    // Refresh the open window when sliders or center changes
+    if (this.clickedLocationWindow) {
+      this.clickedLocationWindow.close();
+      for (const circle of this.circles) {
+        if (circle.getCenter().equals(this.clickedLatLng)) {
+          this.clickedCircle = circle;
+          break;
+        }
+      }
+      const e = {
+        latLng: this.clickedLatLng
+      };
+      google.maps.event.trigger(this.clickedCircle, 'click', e);
+    }
   }
+
+
 
   handleLocationError(browserHasGeolocation: boolean, infoWindow: any, pos: any) {
     infoWindow.setPosition(pos);
--- src/app/screens/screens.module.ts
+++ src/app/screens/screens.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSliderModule,
+    FormsModule
   ],
   exports: [ DashboardComponent ]
 })
