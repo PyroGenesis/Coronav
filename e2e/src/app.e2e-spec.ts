1a048454fe02d7a7ef8028a4d1dcca149d89bccb
Burhanuddin Lakdawala <blakdawa@uci.edu>
2020-03-29 05:50:55 -0700

    Coronav is here!

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('quaranteam-web app is running!');
+    expect(page.getTitleText()).toEqual('Coronav app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/quaranteam-web'),
+      dir: require('path').join(__dirname, './coverage/coronav-pwa'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
